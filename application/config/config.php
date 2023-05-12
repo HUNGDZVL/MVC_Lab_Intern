fdf892ec360bc0ce68e52564df65765cee8ca00a
HUNDZVL <nguyendoanquochung9a1@gmail.com>
2023-05-12 10:15:48 +0700

    updatecode config

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-//$config['base_url'] = 'http://localhost/LabMVC/';
-$config['base_url'] = 'http://quochung.intern.midvietnam.com/LabMVC/';
+$config['base_url'] = 'http://localhost/LabMVC/';
+$config['base_url'] = 'http://quochung.intern.midvietnam.com/';
 
 /*
 |--------------------------------------------------------------------------
